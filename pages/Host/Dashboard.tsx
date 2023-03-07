d4b5c4d3c493941e10347f2b66e2521c5876ff4a
eduardneague <eduard.neague1337@gmail.com>
2023-03-07 20:19:40 +0200

    fixed tailwind bug

--- pages/Host/Dashboard.tsx
+++ pages/Host/Dashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard: React.FC = (): JSX.Element => {
   return (
-    <div>Dashboard</div>
+    <div className = "text-black">hello</div>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   "./index.html",
   "./src/*.{js,ts,jsx,tsx}",
   "./components/*.{js,ts,jsx,tsx}",
-  "./pages/*.{js,ts,jsx,tsx}"],
+  "./pages/**/*.{js,ts,jsx,tsx}",],
 
   theme: {
     extend: {
