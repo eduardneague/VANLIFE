20af6b18c7580f5cab64f6f53d10e558cadd69b7
eduardneague <eduard.neague1337@gmail.com>
2023-03-08 19:06:05 +0200

    added no rented vans screen

--- pages/Host/Host Vans/HostVans.tsx
+++ pages/Host/Host Vans/HostVans.tsx
@@ -3,6 +3,7 @@ import {Context} from '../../../src/ContextProvider'
 import RentedVan from '../../../components/RentedVan'
 import {nanoid} from 'nanoid'
 import {VanType} from '../../../types/VanType'
+import {Link} from 'react-router-dom'
 
 const HostVans: React.FC = (): JSX.Element => {
 
@@ -25,7 +26,13 @@ const rentedVanElements = rentedVans.map((van: VanType) => {
     <div className = "w-full h-[40rem] flex justify-center">
         <div className="div FLEX_CONTAINER flex flex-col h-full w-10/12">
         <h1 className = "font-bold text-3xl mb-5">Your rented vans</h1>
-            {rentedVanElements}
+            {rentedVans.length === 0 ? 
+            <div>
+                <h1 className = "text-xl font-normal">You haven't rented any vans yet!</h1>
+                <Link to = "/vans" 
+                className = "flex mt-3 justify-center items-center bg-orange-500 text-white w-32 rounded-md shadow h-10 text-lg font-bold">Rent Vans</Link>
+            </div> 
+            : rentedVanElements}
         </div>
     </div>
   )
